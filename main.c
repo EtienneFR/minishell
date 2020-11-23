695cb8f4214bae919ded2e36d64f96d9379772b9
Etienne Dayre <etiennedayre10@gmail.com>
2020-11-23 22:25:59 +0100

    last update

--- main.c
+++ main.c
@@ -16,32 +16,32 @@
 //1.-----------------------Lire entree-------------------------
 char *lire_ligne(void)
 {
-  int indice = 0;
-  char *buffer = malloc(sizeof(char) * 1024);
-  int a;
-
-  while (1) {
-    // Lire une ligne
-    a = getchar();
-
-    // Si retour a la ligne, derniere valeur à null
-    if (a == '\n') {
-      buffer[indice] = '\0';
-      return buffer;
-    } else {
-      buffer[indice] = a;
+    int indice = 0;
+    char *buffer = malloc(sizeof(char) * 1024);
+    int a;
+    
+    while (1) {
+        // Lire une ligne
+        a = getchar();
+        
+        // Si retour a la ligne, derniere valeur à null
+        if (a == '\n') {
+            buffer[indice] = '\0';
+            return buffer;
+        } else {
+            buffer[indice] = a;
+        }
+        //Incrementation de l indice dans le tableau
+        indice++;
+        
     }
-      //Incrementation de l indice dans le tableau
-      indice++;
-
-  }
 }
 
 //2.----- Construction de argv (tableau de chaine) -----------
-char **construction_tableau_chaine(char *line, char**envp)
+char **construction_tableau_chaine(char *line)
 {
     int buffersize = 64, indice = 0;
-    char **elements = malloc(buffersize * sizeof(char*)); //Allocation mémoire
+    char **elements = malloc(buffersize * sizeof(char*)); //Allocation memoire
     char *element;
     
     //Premier decoupage
@@ -51,13 +51,12 @@ char **construction_tableau_chaine(char *line, char**envp)
     while (element != NULL) {
         elements[indice] = element;
         indice++;
-
-        //Deuxieme découpage
+        
+        //Deuxieme decoupage
         element = strtok(NULL, DELIMITATION);
     }
-
+    
     elements[indice] = NULL;
-
     return elements;
 }
 
@@ -76,13 +75,13 @@ int executer_ligne(char **args, char **envp)
     
     //Si la commande est cd
     if (!strcmp(args[0], "cd")){
-
+        
         repertoire_absolu = getcwd(buffer, sizeof(buffer));
         
         //Concaténation de chaines de caracteres
         repertoire = strcat(repertoire_absolu, "/");
         repertoire_destination = strcat(repertoire, args[1]);
-
+        
         chdir(repertoire_destination);
         
     } else {    //Pour tout autre commande, autre de cd
@@ -104,24 +103,24 @@ int executer_ligne(char **args, char **envp)
         } else {
             // Processus pere
             do {
-            
-            //Attente du processus fils
-            wpid = waitpid(pid, &status, WUNTRACED);
                 
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status)); //Renvoie true si fils terminée&& terminé a cause signal
+                //Attente du processus fils
+                wpid = waitpid(pid, &status, WUNTRACED);
+                
+            } while (!WIFEXITED(status) && !WIFSIGNALED(status)); //Renvoie true si fils terminee && termine a cause signal
         }
     }
     return 1;
- 
+    
 }
 
 //------------------------ Main ------------------------------
-int main(int argc, char **argv, char *envp[]) {
-
+int main(int argc,char *argv[], char *envp[]) {
+    
     //Etapes du mini shell
-    //1. Lire la ligne en entrée
+    //1. Lire la ligne en entree
     //2. Transformer ligne en tableau de chaine grace a strtok()
-    //3. Executer la commande
+    //3. Executer la commande (fork() puis execve())
     char *chaine;
     char **args;
     int execute;
@@ -130,35 +129,34 @@ int main(int argc, char **argv, char *envp[]) {
         
         //---------- Affichage dernier dossier courant------------
         char str_destination[1024];
-
+        
         char dossier_courant[1024] = { 0 };
         char dossier_courant_char = '\0';
         int  index = 0;
         int  dernier_slash_index = 0;
-
+        
         getcwd(dossier_courant, 1024);
-
+        
         while ((dossier_courant_char = dossier_courant[index]) != '\0') {
             if (dossier_courant_char == '/') {
                 dernier_slash_index = index;
             }
-
+            
             index++;
         }
-
+        
         strcpy(str_destination, dossier_courant + dernier_slash_index + 1);
         //--------------------------------------------------------
         
         printf("%s > ", str_destination);
         chaine = lire_ligne();
-        args = construction_tableau_chaine(chaine, envp);
+        args = construction_tableau_chaine(chaine);
         execute = executer_ligne(args, envp);
-
-        //Libération de la mémoire
+        
+        //Liberation de la memoire
         free(chaine);
         free(args);
     } while (execute);
-
+    
     return EXIT_SUCCESS;
-
 }
